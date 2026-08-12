484b5a24f5a5e5de2980c41cffded7f9d222004e
Haikal Fikri <haikaljr1245@gmail.com>
2026-08-13 04:30:23 +0700

    Initial Commit

---
+++ backend/store/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import JsonResponse
+
+
+# Create your views here.
+def home(request):
+    data = {
+        'message': 'Welcome to store'
+    }
+
+    return JsonResponse(data)
