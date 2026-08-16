dfbfd3eaaf7cda2e43cf1f64635dfcaa892c0992
Haikal Fikri <haikaljr1245@gmail.com>
2026-08-16 07:27:23 +0700

    Cart functionality(backend)

--- backend/store/admin.py
+++ backend/store/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, Product, UserProfile, Order, OrderItem
+from .models import Category, Product, UserProfile, Order, OrderItem, Cart, CartItem
 
 # Register your models here.
 admin.site.register(Category)
@@ -7,3 +7,5 @@ admin.site.register(Product)
 admin.site.register(UserProfile)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Cart)
+admin.site.register(CartItem)
